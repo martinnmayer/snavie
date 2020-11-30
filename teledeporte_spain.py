4fd8f99c9e2709f1a28c0e8c22a12cf9996672b6
unknown <martinnmayer@gmail.com>
2020-11-30 18:41:18 -0300

    Revert "user agent for teledeporte"
    This reverts commit 3f8821294294e4710ffc86ecf11714b93c9654ca.

--- teledeporte_spain.py
+++ teledeporte_spain.py
@@ -21,8 +21,8 @@ def run():
         date_url = date.strftime("%d%m%Y")
 
         url = "https://www.rtve.es/contenidos/sala-de-comunicacion/programacion-descargable/TELEDEPORTE_{}.html".format(date_url)
-        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-        response = requests.get(url,headers=headers)
+
+        response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
 
         result_list = []
